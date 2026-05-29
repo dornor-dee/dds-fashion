a8b4ef0346d1691001fcae2ac49af6b6e890545b
Derrick <doubledeestudio1@gmail.com>
2026-05-29 11:12:45 -0500

    Add page transitions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Register from "./pages/Register";
 import Admin from "./pages/Admin";
 import ProductDetails from "./pages/ProductDetails";
 import MyOrders from "./pages/MyOrders";
-import Wishlist from "./pages/Wishlist.jsx";
+import Wishlist from "./pages/Wishlist";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Categories from "./pages/Categories";
@@ -15,23 +15,150 @@ import Contact from "./pages/Contact";
 import AdminDashboard from "./pages/AdminDashboard";
 import Checkout from "./pages/Checkout";
 
+import PageTransition from "./components/PageTransition";
+
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<DDSFashionHomepage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/product/:id" element={<ProductDetails />} />
-      <Route path="/myorders" element={<MyOrders />} />
-      <Route path="/wishlist" element={<Wishlist />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/admin-dashboard" element={<AdminDashboard />} />
-      <Route path="/checkout" element={<Checkout />} />
+      {/* HOME PAGE */}
+      <Route
+        path="/"
+        element={
+          <PageTransition>
+            <DDSFashionHomepage />
+          </PageTransition>
+        }
+      />
+
+      {/* OPTIONAL HOME PAGE */}
+      <Route
+        path="/home"
+        element={
+          <PageTransition>
+            <Home />
+          </PageTransition>
+        }
+      />
+
+      {/* SHOP */}
+      <Route
+        path="/shop"
+        element={
+          <PageTransition>
+            <Shop />
+          </PageTransition>
+        }
+      />
+
+      {/* CATEGORIES */}
+      <Route
+        path="/categories"
+        element={
+          <PageTransition>
+            <Categories />
+          </PageTransition>
+        }
+      />
+
+      {/* ABOUT */}
+      <Route
+        path="/about"
+        element={
+          <PageTransition>
+            <About />
+          </PageTransition>
+        }
+      />
+
+      {/* CONTACT */}
+      <Route
+        path="/contact"
+        element={
+          <PageTransition>
+            <Contact />
+          </PageTransition>
+        }
+      />
+
+      {/* LOGIN */}
+      <Route
+        path="/login"
+        element={
+          <PageTransition>
+            <Login />
+          </PageTransition>
+        }
+      />
+
+      {/* REGISTER */}
+      <Route
+        path="/register"
+        element={
+          <PageTransition>
+            <Register />
+          </PageTransition>
+        }
+      />
+
+      {/* PRODUCT DETAILS */}
+      <Route
+        path="/product/:id"
+        element={
+          <PageTransition>
+            <ProductDetails />
+          </PageTransition>
+        }
+      />
+
+      {/* MY ORDERS */}
+      <Route
+        path="/myorders"
+        element={
+          <PageTransition>
+            <MyOrders />
+          </PageTransition>
+        }
+      />
+
+      {/* WISHLIST */}
+      <Route
+        path="/wishlist"
+        element={
+          <PageTransition>
+            <Wishlist />
+          </PageTransition>
+        }
+      />
+
+      {/* ADMIN */}
+      <Route
+        path="/admin"
+        element={
+          <PageTransition>
+            <Admin />
+          </PageTransition>
+        }
+      />
+
+      {/* ADMIN DASHBOARD */}
+      <Route
+        path="/admin-dashboard"
+        element={
+          <PageTransition>
+            <AdminDashboard />
+          </PageTransition>
+        }
+      />
+
+      {/* CHECKOUT */}
+      <Route
+        path="/checkout"
+        element={
+          <PageTransition>
+            <Checkout />
+          </PageTransition>
+        }
+      />
     </Routes>
   );
 }
